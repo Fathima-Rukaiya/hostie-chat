420c69b507560e0eb9fb208187fc36c457e01a57
Rukaiya <rukaiyah678@gmail.com>
2025-12-23 17:09:49 +0530

    changes

--- src/components/StandardUI.tsx
+++ src/components/StandardUI.tsx
@@ -256,10 +256,10 @@ export function StandardUI({
 
   //
   //https://hostingate-client.vercel.app/sign-in https://app.hostingate.com/dashboard/profile
-  // const API_BASE_URL = "https://app.hostingate.com/api/clientCustomerChatBox";
+  const API_BASE_URL = "https://app.hostingate.com/api/clientCustomerChatBox";
   //const API_BASE_URL = "https://app.hostie.ai/api/clientCustomerChatBox";
 
-  const API_BASE_URL = "http://localhost:3000/api/clientCustomerChatBox";
+  // const API_BASE_URL = "http://localhost:3000/api/clientCustomerChatBox";
   useEffect(() => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [chatHistory]);
