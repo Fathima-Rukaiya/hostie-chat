fe294d194f13219ce1021cabc3dd587794cac66f
Rukaiya <rukaiyah678@gmail.com>
2025-12-24 10:08:11 +0530

    changes

--- src/components/chatUI.tsx
+++ src/components/chatUI.tsx
@@ -179,7 +179,7 @@ export function ChatUI({ apiKey,
                 {isOpen && (
                     // {botName}
                     <div
-                        className="absolute bottom-full mb-3 right-0 w-80 p-0 shadow-2xl rounded-xl transition-all duration-200">
+                        className="absolute bottom-full mb-3 left-0 w-80 p-0 shadow-2xl rounded-xl transition-all duration-200">
                         <StandardUI apiKey={apiKey} shadowContainer={shadowContainer} botIcon={botIcon || ""} botName={botName} gradient={gradient} darkGradient={darkModeGradient} borderColor={borderColor} darkBorderColor={darkBorderColor}
                             greeting={greeting}
                             introduction={introduction}
