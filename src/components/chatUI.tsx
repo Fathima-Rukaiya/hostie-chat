cd3fb73a8a46cef08c9905b855c985551601988a
Rukaiya <rukaiyah678@gmail.com>
2025-12-24 09:44:29 +0530

    changes

--- src/components/chatUI.tsx
+++ src/components/chatUI.tsx
@@ -5,6 +5,7 @@ import { StandardUI } from "./StandardUI";
 import { Bot } from "lucide-react";
 import { ThemeProvider, useTheme } from "next-themes";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { motion } from "framer-motion";
 
 export function ChatUI({ apiKey,
     shadowContainer,
@@ -169,10 +170,35 @@ export function ChatUI({ apiKey,
                     ) : (
                         <Bot strokeWidth={1.75} size={22} />
                     )}
-                    <span className="font-semibold text-sm">Ask {botName}!
+                    <span className="font-semibold text-sm">Ask {botName}
 
                     </span>
                 </button>
+
+                   <motion.svg
+              className="absolute inset-0 w-full h-full"
+              viewBox="0 0 100 100"
+              animate={{ rotate: 360 }}
+              transition={{
+                duration: 20,
+                repeat: Number.POSITIVE_INFINITY,
+                ease: "linear",
+              }}
+              style={{ transform: "scale(1.6)" }}
+            >
+              <defs>
+                <path
+                  id="circle"
+                  d="M 50, 50 m -38, 0 a 38,38 0 1,1 76,0 a 38,38 0 1,1 -76,0"
+                />
+              </defs>
+
+              <text className="text-sm fill-white/80 instrument">
+                <textPath href="#circle" startOffset="0%">
+                  • Club Choiseul Arabian Gulf • Club Choiseul Arabian Gulf •
+                </textPath>
+              </text>
+            </motion.svg>
 
                 {isOpen && (
                     // {botName}
